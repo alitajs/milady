bf8e1e813a1bfeb305b9d396a2b141cc06340943
xiaohuoni <448627663@qq.com>
2019-06-27 15:40:10 +0800

    development environment

--- bin/codegen.js
+++ bin/codegen.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
-const yParser =require('yargs-parser');
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const yParser = require('yargs-parser');
+
 const argv = yParser(process.argv.slice(2));
 const [url] = argv._;
 const nodeVersion = process.versions.node;
@@ -8,10 +11,11 @@ const major = versions[0];
 const minor = versions[1];
 
 if (major * 10 + minor * 1 < 65) {
-    console.log(`Node version must >= 6.5, but got ${major}.${minor}`);
-    process.exit(1);
+  // eslint-disable-next-line no-console
+  console.log(`Node version must >= 6.5, but got ${major}.${minor}`);
+  process.exit(1);
 }
 
 const codegen = require('../lib/index').default;
 
-codegen(url,argv)
+codegen(url, argv);
