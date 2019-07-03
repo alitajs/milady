6d6c3b4e64647b0efcb06e0ceb694dc8f9ed7efe
DaoYuan <txp1035@gmail.com>
2019-07-03 22:55:42 +0800

    fix：strToKey

--- src/utils/handelStr.test.ts
+++ src/utils/handelStr.test.ts
@@ -1,5 +1,10 @@
-import { handelRef } from './handelStr';
+import { handelRef, strToKey } from './handelStr';
 
 test('handelRef', () => {
   expect(handelRef('#/definitions/aa')).toEqual('aa');
+});
+test('strToKey', () => {
+  expect(strToKey('#/definitions/aa')).toEqual('definitionsaa');
+  expect(strToKey('你会12aa')).toEqual('你会12aa');
+  expect(strToKey('')).toEqual('');
 });
